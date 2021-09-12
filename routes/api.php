daab56175d170ab1f4ca5a9ea53338ecb7d93ab3
Alexander <antidze.sasha@gmail.com>
2021-09-12 20:58:57 +0400

    fixing api routes

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/timeline', 'App\Http\Controllers\Api\Timeline\TimelineController@index');
-Route::post('/tweets', 'App\Http\Controllers\Api\Tweets\TweetController@store');
+Route::get('/media/types', 'App\Http\Controllers\Api\Media\MediaTypesController@index');
 Route::get('/tweets', 'App\Http\Controllers\Api\Tweets\TweetController@index');
-Route::post('/tweets/{tweet}/likes', 'App\Http\Controllers\Api\Tweets\TweetLikeController@store');
-Route::delete('/tweets/{tweet}/likes', 'App\Http\Controllers\Api\Tweets\TweetLikeController@destroy');
-Route::post('/tweets/{tweet}/replies', 'App\Http\Controllers\Api\Tweets\TweetReplyController@store');
 
+Route::post('/tweets', 'App\Http\Controllers\Api\Tweets\TweetController@store');
+Route::post('/tweets/{tweet}/likes', 'App\Http\Controllers\Api\Tweets\TweetLikeController@store');
+Route::post('/tweets/{tweet}/replies', 'App\Http\Controllers\Api\Tweets\TweetReplyController@store');
 Route::post('/tweets/{tweet}/retweets', 'App\Http\Controllers\Api\Tweets\TweetRetweetController@store');
+Route::post('/tweets/{tweet}/quotes', 'App\Http\Controllers\Api\Tweets\TweetQuoteController@store');
+Route::post('/media', 'App\Http\Controllers\Api\Media\MediaController@store');
+
+Route::delete('/tweets/{tweet}/likes', 'App\Http\Controllers\Api\Tweets\TweetLikeController@destroy');
 Route::delete('/tweets/{tweet}/retweets', 'App\Http\Controllers\Api\Tweets\TweetRetweetController@destroy');
 
-Route::post('/tweets/{tweet}/quotes', 'App\Http\Controllers\Api\Tweets\TweetQuoteController@store');
 
-
-Route::get('/media/types', 'App\Http\Controllers\Api\Media\MediaTypesController@index');
-Route::post('/media', 'App\Http\Controllers\Api\Media\MediaController@store');
 
 
